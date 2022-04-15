8d2a20c8029a10537af43b1118933555c6b0bbf8
Damian Świstowski <damian@swistowski.org>
2022-04-15 17:21:11 +0100

    add version which assume string is ascii

--- chapter-01-arrays-and-strings/exercise_01_is_unique.py
+++ chapter-01-arrays-and-strings/exercise_01_is_unique.py
@@ -4,7 +4,8 @@ Implement an algorithm to determine if a string hasll all unique characters
 import pytest
 
 
-def is_unique(input: str):
+def is_unique(input: str) -> bool:
+    """simple version"""
     while input:
         needle, *input = input
         if needle in input:
@@ -12,14 +13,34 @@ def is_unique(input: str):
     return True
 
 
-@pytest.mark.parametrize("input,expected", [
+def is_unique_ascii(input: str) -> bool:
+    """ascii version of is unique. Assume input string is ascii"""
+    letters = list(input.encode())
+    in_string = [False for _ in range(256)]
+    for letter in letters:
+        if in_string[letter]:
+            return False
+        in_string[letter] = True
+    return True
+
+
+
+params = [
     ('', True),
     ('a', True),
     ('abc', True),
     ('abca', False)
-])
-def test_is_unique(input, expected):
-    assert is_unique(input) == expected
+]
+
+
+@pytest.fixture(params=[is_unique, is_unique_ascii])
+def function_under_test(request):
+    return request.param
+
+
+@pytest.mark.parametrize("input,expected", params)
+def test_is_unique(input, expected, function_under_test):
+    assert function_under_test(input) == expected
 
 
 if __name__ == "__main__":
